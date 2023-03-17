4043b752bf6bc22a14c9f3ef49d73c58e7f3fe0f
PabloMunizMontoya <119509495+PabloMunizMontoya@users.noreply.github.com>
2023-03-17 18:21:09 -0500

    creamos el Header y usamos materilize, para el css, los cambios para agregar materilize estan en public/ index html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import Header from './components/Header';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>hola</h1>
-    </div>
+    <Fragment>
+      <Header
+        /* 1. ponemos la prop al header */
+        titulo = 'Clima React APP'
+      >
+
+      </Header>
+      
+    </Fragment>
+    
   );
 }
 
